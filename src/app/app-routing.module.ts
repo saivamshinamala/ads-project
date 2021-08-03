9c8909e37166ada22892c3f8eeacc2cd43d27c7b
NithishGajula <nithishg1199@gmail.com>
2021-08-03 22:32:59 +0530

    Styling on Card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardComponent } from './card/card.component';
 import { CreateAdsComponent } from './create-ads/create-ads.component';
 import { PromoteAdsComponent } from './promote-ads/promote-ads.component';
 
 const routes: Routes = [
   {path: "", component: CreateAdsComponent},
-  {path: "promote-ad", component: PromoteAdsComponent}
+  {path: "promote-ad", component: PromoteAdsComponent},
+  {path:"card", component: CardComponent}
 ];
 
 @NgModule({
